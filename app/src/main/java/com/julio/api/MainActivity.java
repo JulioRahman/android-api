ea498a0b3b8274a184ba1e5a0dec7093aedffe2f
Julio Rahman <julio.rahman@gmail.com>
2019-10-25 14:42:18 +0700

    feat: displays response to textView

--- app/src/main/java/com/julio/api/MainActivity.java
+++ app/src/main/java/com/julio/api/MainActivity.java
@@ -2,6 +2,7 @@ package com.julio.api;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,10 +17,15 @@ import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvNama, tvEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvNama = findViewById(R.id.text_nama);
+        tvEmail = findViewById(R.id.text_email);
 
         Retrofit retrofit = APIClient.getClient();
         EndPoint endPoint = retrofit.create(EndPoint.class);
@@ -27,7 +33,11 @@ public class MainActivity extends AppCompatActivity {
         responseCall.enqueue(new Callback<Users>() {
             @Override
             public void onResponse(Call<Users> call, Response<Users> response) {
-                Log.d("Eudeka!", "onResponse: " + response.body().data.get(0).lastName);
+                tvNama.setText("Nama: " +
+                        response.body().data.get(0).firstName + " " +
+                        response.body().data.get(0).lastName);
+                tvEmail.setText("Email: " +
+                        response.body().data.get(0).email);
             }
 
             @Override
